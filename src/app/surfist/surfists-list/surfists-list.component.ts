f4b2979346b998beac6041f1357d5edd8869cf5b
CCnova <caiocnova1@gmail.com>
2020-03-02 08:47:57 -0300

    Add bootstrap 4 to the project

--- src/app/surfist/surfists-list/surfists-list.component.ts
+++ src/app/surfist/surfists-list/surfists-list.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { SurfistService } from 'src/app/surfist.service';
+import { Surfist } from '../surfist.model';
 
 @Component({
   templateUrl: './surfists-list.component.html',
   styleUrls: ['./surfists-list.component.css']
 })
 export class SurfistsListComponent implements OnInit {
+  surfists: Surfist[];
 
-
-  constructor(private _surfistService : SurfistServiceService) {}
+  constructor(private _surfistService : SurfistService) {}
 
   ngOnInit() {
-
+    this.surfists = this._surfistService.getAllSurfists();
   }
 }
