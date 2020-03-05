87a7257751a70afdc1aad8052e69d7584c96bf08
CCnova <caiocnova1@gmail.com>
2020-03-05 08:18:35 -0300

    Add factories and seeders for database tables

---
+++ backend/database/seeds/BaterySeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BaterySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
